2a0e8fcc9a5078fee13cdb95d7f8e9bf08b180dd
Gergely Benko <gergely.benko93@gmail.com>
2017-11-06 22:51:16 +0100

    Adding a compass

--- src/jhonny5/jhonny5_main.cpp
+++ src/jhonny5/jhonny5_main.cpp
@@ -9,34 +9,45 @@ int main(int argc, char **argv)
     ros::Publisher cmd_vel_pub_;
     ros::Rate loop_rate(10);
     geometry_msgs::Twist base_cmd;
-    
+
     ros::Subscriber finishTileCheck = nh.subscribe("/jhonny5/finishtileinfo", 1, finishTileCallback);
     ros::Subscriber wallProximityCheck = nh.subscribe("/jhonny5/wallproximityinfo", 1, wallProximityCallback);
     ros::Subscriber crossingLeftCheck = nh.subscribe("/jhonny5/crossingleftinfo", 1, crossingLeftCallback);
     ros::Subscriber crossingRightCheck = nh.subscribe("/jhonny5/crossingrightinfo", 1, crossingRightCallback);
     ros::Subscriber laserScanCheck = nh.subscribe<sensor_msgs::LaserScan>("/jhonny5/laser/scan",10,&laserScanCallback);
+    ros::Subscriber odometryCheck = nh.subscribe<nav_msgs::Odometry>("odom", sizeof(nav_msgs::Odometry), &odometryCallback);
     cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
     base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
 
+    bool turnOnce = true;
+
     while(ros::ok())
     {
-        if(avgrange > 2.5)
-        {
-            base_cmd.angular.z = 1.00;
-            base_cmd.linear.x = base_cmd.linear.y = 0;
-        }
-        else
-        {
-            base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
+        ros::spinOnce();
+        // if(avgrange > 2.5)
+        // {
+        //     base_cmd.angular.z = 1.00;
+        //     base_cmd.linear.x = base_cmd.linear.y = 0;
+        // }
+        // else
+        // {
+        //     base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
+        // }
+
+        if (turnOnce) {
+            base_cmd = turn_right();
+            if (base_cmd.linear.x == 0.0) {
+              robot_direction = get_right_dir(robot_direction);
+              turnOnce = false;
+            }
         }
 
         Jhonny5_state_machine();
-        Jhonny5_execute_order();        
+        Jhonny5_execute_order();
 
         cmd_vel_pub_.publish(base_cmd);
 
-        ros::spinOnce();
         loop_rate.sleep();
     }
     return 0;
@@ -46,6 +57,7 @@ void Jhonny5_init(void)
 {
     RobotState = init;
     RobotStateLastLoop = RobotState;
+    robot_direction = DOWN;
     FinishTileDetected = False;
     WallDetectedFront = False;
     CrossingDetectedLeft = False;
@@ -56,7 +68,7 @@ void Jhonny5_init(void)
 void Jhonny5_state_machine(void)
 {
     RobotStateLastLoop = RobotState;
-    
+
     /* State switch conditions */
     switch(RobotState)
     {
@@ -119,7 +131,7 @@ void Jhonny5_state_machine(void)
             if(RobotState == position_determination)
             break;
         default:
-            break;   
+            break;
         }
     }
 }
@@ -157,8 +169,67 @@ void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
      {
          lsranges[i] = scan->ranges[i];
          sum += lsranges[i];
-         printf("Range data %d : %1.8f\n",i, lsranges[i]);
+        //  printf("Range data %d : %1.8f\n",i, lsranges[i]);
      }
      avgrange = sum/10;
-     printf("Average: %1.8f\n",avgrange);
+    //  printf("Average: %1.8f\n",avgrange);
+}
+
+void odometryCallback(const nav_msgs::Odometry::ConstPtr& odom) {
+  robot_orientation.z = odom->pose.pose.orientation.z;
+  robot_orientation.w = odom->pose.pose.orientation.w;
+}
+
+geometry_msgs::Twist turn_left(void) {
+  direction   dir_target = get_left_dir(robot_direction);
+  orientation or_target  = get_orienation_from_direction(dir_target);
+  geometry_msgs::Twist twist;
+
+  if ((fabs(or_target.z - robot_orientation.z) < EPSILON && fabs(or_target.w - robot_orientation.w) < EPSILON) ||
+      (fabs(or_target.z + robot_orientation.z) < EPSILON && fabs(or_target.w + robot_orientation.w) < EPSILON)) {
+    printf("Stop turning...\n");
+    twist.linear.x = 0.0;
+  } else {
+    twist.linear.x = 0.1;
+  }
+  return twist;
+}
+
+geometry_msgs::Twist turn_right(void) {
+  direction   dir_target = get_right_dir(robot_direction);
+  orientation or_target  = get_orienation_from_direction(dir_target);
+  geometry_msgs::Twist twist;
+
+  if ((fabs(or_target.z - robot_orientation.z) < EPSILON && fabs(or_target.w - robot_orientation.w) < EPSILON) ||
+      (fabs(or_target.z + robot_orientation.z) < EPSILON && fabs(or_target.w + robot_orientation.w) < EPSILON)) {
+    printf("Stop turning...\n");
+    twist.linear.x = 0.0;
+  } else {
+    twist.linear.x = -0.1;
+  }
+  return twist;
+}
+
+orientation get_orienation_from_direction(direction d) {
+  switch (d) {
+    case DOWN:
+      return orientation(sin(2.0 * M_PI / 2.0), cos(2.0 * M_PI / 2.0));
+
+    case LEFT:
+      return orientation(sin(1.5 * M_PI / 2.0), cos(1.5 * M_PI / 2.0));
+
+    case UP:
+      return orientation(sin(1.0 * M_PI / 2.0), cos(1.0 * M_PI / 2.0));
+
+    case RIGHT:
+      return orientation(sin(0.5 * M_PI / 2.0), cos(0.5 * M_PI / 2.0));
+  }
+}
+
+direction get_left_dir(direction d) {
+  return (direction)((d + 3) % 4);
+}
+
+direction get_right_dir(direction d) {
+  return (direction)((d + 1) % 4);
 }
--- src/jhonny5/jhonny5_main.h
+++ src/jhonny5/jhonny5_main.h
@@ -1,12 +1,16 @@
 #ifndef JHONNY5_MAIN_HH
 #define JHONNY5_MAIN_HH
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "ros/ros.h"
-#include "std_msgs/Bool.h" 
-#include "std_msgs/String.h" 
+#include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
 #include <geometry_msgs/Twist.h>
 #include <actionlib/server/simple_action_server.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
 #include <gazebo/gazebo.hh>
 #include <sstream>
 
@@ -19,12 +23,31 @@ typedef enum robot_states_e
     error_state
 } robot_states_t;
 
+struct orientation {
+  float z; // sin (theta / 2)
+  float w; // cos (theta / 2)
+
+  orientation(void) : w(0.0f), z(0.0f) {}
+  orientation(float _z, float _w) : z(_z), w(_w) {}
+};
+
+enum direction {
+  DOWN,
+  LEFT,
+  UP,
+  RIGHT
+};
+
 #define False (0)
 #define True  (1)
 //typedef actionlib::SimpleActionServer<jhonny5::MoveOrder> MA;
 
+#define EPSILON 0.005
+
 robot_states_t RobotState;
 robot_states_t RobotStateLastLoop;
+orientation    robot_orientation;
+direction      robot_direction;
 bool           FinishTileDetected;
 bool           WallDetectedFront;
 bool           RobotStopped;
@@ -38,10 +61,16 @@ void Jhonny5_init(void);
 void Jhonny5_input_processing(void);
 void Jhonny5_state_machine(void);
 void Jhonny5_execute_order(void);
+geometry_msgs::Twist turn_left(void);
+geometry_msgs::Twist turn_right(void);
+orientation get_orienation_from_direction(direction d);
+direction get_left_dir(direction d);
+direction get_right_dir(direction d);
 /* Callback functions */
 void finishTileCallback(const std_msgs::BoolConstPtr& str);
 void wallProximityCallback(const std_msgs::BoolConstPtr& str);
 void crossingLeftCallback(const std_msgs::BoolConstPtr& str);
 void crossingRightCallback(const std_msgs::BoolConstPtr& str);
 void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+void odometryCallback(const nav_msgs::Odometry::ConstPtr& odom);
 #endif /* JHONNY5_MAIN_HH */
