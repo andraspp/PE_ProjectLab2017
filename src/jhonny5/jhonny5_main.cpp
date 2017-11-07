3203905c193152f6d157e7faba1c3c94c757e1ed
Andras Papp <andraspp@gmail.com>
2017-11-07 16:58:17 +0100

    Added state machine logic, but turning is buggy for some reason, help :-(

--- src/jhonny5/jhonny5_main.cpp
+++ src/jhonny5/jhonny5_main.cpp
@@ -6,59 +6,36 @@ int main(int argc, char **argv)
 
     Jhonny5_init();
     ros::NodeHandle nh;
-    ros::Publisher cmd_vel_pub_;
+ 
     ros::Rate loop_rate(10);
     geometry_msgs::Twist base_cmd;
     
-    ros::Subscriber finishTileCheck = nh.subscribe("/jhonny5/finishtileinfo", 1, finishTileCallback);
-    ros::Subscriber wallProximityCheck = nh.subscribe("/jhonny5/wallproximityinfo", 1, wallProximityCallback);
-    ros::Subscriber crossingLeftCheck = nh.subscribe("/jhonny5/crossingleftinfo", 1, crossingLeftCallback);
-    ros::Subscriber crossingRightCheck = nh.subscribe("/jhonny5/crossingrightinfo", 1, crossingRightCallback);
-    ros::Subscriber laserScanCheck = nh.subscribe<sensor_msgs::LaserScan>("/jhonny5/laser/scan",10,&laserScanCallback);
+    ros::topic::waitForMessage<nav_msgs::Odometry>("/jhonny5/odom");
+    ros::Subscriber sub = nh.subscribe("/jhonny5/odom",1000,getOdom);
+    ros::Subscriber laserScanFrontCheck = nh.subscribe<sensor_msgs::LaserScan>("/jhonny5/laser/scan",10,&laserScanFrontCallback);
+    ros::Subscriber laserScanLeftCheck = nh.subscribe<sensor_msgs::LaserScan>("/jhonny5/laser/scan_left",10,&laserScanLeftCallback);
+    ros::Subscriber laserScanRightCheck = nh.subscribe<sensor_msgs::LaserScan>("/jhonny5/laser/scan_right",10,&laserScanRightCallback);
+ 
     cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
-    base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
+    set_velocities(stopSpeed, stopSpeed);
 
     while(ros::ok())
     {
-        if(   (avgrange > 1.2)
-           && (TurnFinished == 1)
-          )
-        {
-            base_cmd.angular.z = 1.00;
-            base_cmd.linear.x = base_cmd.linear.y = 0;
-            StopTimer = 30;
-            TurnTimer = 100;
-        }
-        else
-        {
-            base_cmd.linear.x = base_cmd.linear.y = base_cmd.angular.z = 0;
-            if(StopTimer > 0)
-            {
-                StopTimer--;
-            }
-            else
-            {
-                if(TurnTimer > 0)
-                {
-                    base_cmd.linear.x = 0.25;
-                    base_cmd.linear.y = base_cmd.angular.z = 0;
-                    TurnTimer--;
-                    TurnFinished = 0;
-                }
-                else
-                {
-                    TurnFinished = 1;
-                }
-                
-            }
-        }
+        Jhonny5_state_machine();      
 
-        Jhonny5_state_machine();
-        Jhonny5_execute_order();        
-
-        cmd_vel_pub_.publish(base_cmd);
-
+        ROS_INFO("Front dist average: %1.8f",AvgFrontRange);
+        ROS_INFO("WallFront: %d", WallDetectedFront);
+        ROS_INFO("Left dist average: %1.8f",AvgLeftRange);
+        ROS_INFO("CrossLeft: %d", CrossingDetectedLeft);
+        ROS_INFO("Right dist average: %1.8f",AvgRightRange);
+        ROS_INFO("CrossRight: %d", CrossingDetectedRight);
+        ROS_INFO("RobotState: %d", RobotState);
+        ROS_INFO("Yaw current: %1.8f", YawCurrent);
+        ROS_INFO("ChosenPath: %d", ChosenPath);
+        ROS_INFO("Path has been chosen: %d", PathSelected);
+        ROS_INFO("Path update suppressed: %d", SuppressPathUpdate);
+        ROS_INFO("Turn finished: %d\n", TurnFinished);
         ros::spinOnce();
         loop_rate.sleep();
     }
@@ -69,12 +46,15 @@ void Jhonny5_init(void)
 {
     RobotState = init;
     RobotStateLastLoop = RobotState;
-    FinishTileDetected = False;
-    WallDetectedFront = False;
-    CrossingDetectedLeft = False;
-    CrossingDetectedRight = False;
-    LocationCheckProcessDone = False;
-    TurnFinished = True;
+    WallDetectedFront = false;
+    CrossingDetectedLeft = false;
+    CrossingDetectedLeftLL = CrossingDetectedLeft;
+    CrossingDetectedRight = false;
+    CrossingDetectedRightLL = CrossingDetectedRight;
+    TurnFinished = false;
+    SuppressPathUpdate = false;
+    PathSelected = false;
+    ChosenPath = none;
 }
 
 void Jhonny5_state_machine(void)
@@ -88,101 +68,270 @@ void Jhonny5_state_machine(void)
         RobotState = position_determination;
         break;
     case position_determination:
-        if(FinishTileDetected == True)
+        if(   (TurnFinished == true)
+           && (ChosenPath   != none)
+           && (WallDetectedFront == false)
+          )
         {
-            RobotState = end_state;
-        }
-        else if(LocationCheckProcessDone == True)
-        {
-            LocationCheckProcessDone = False;
             RobotState = moving;
         }
-        break;
-    case moving:
-        if(   (WallDetectedFront == True)
-           || (CrossingDetectedLeft == True)
-           || (CrossingDetectedRight == True)
-           || (FinishTileDetected == True)
-          )
+        else
         {
             RobotState = position_determination;
         }
         break;
-    case end_state:
+    case moving:
+        if(   (SuppressPathUpdate == false)
+           && (   (WallDetectedFront == true)
+               || (CrossingDetectedRight == true)
+              )
+          )
+        {
+            RobotState = position_determination;
+        }
+        else
+        {
+            RobotState = moving;
+        }
         break;
-    case error_state:
     default:
-        RobotState = error_state;
+        RobotState = position_determination;
         break;
     }
 
     /* State dependent actions */
-    if(RobotStateLastLoop == RobotStateLastLoop)
+
+    switch(RobotState)
     {
-        switch(RobotState)
-        {
-        case init:
-            break;
-        case position_determination:
-            break;
-        case moving:
-            break;
-        case end_state:
-            break;
-        case error_state:
-        default:
-            ros::shutdown();
-            break;
+    case position_determination:
+        if(PathSelected == false)
+        {            
+            if(CrossingDetectedRight == true)
+            {
+                ChosenPath = right;
+            }
+            else if(WallDetectedFront == false)
+            {
+                ChosenPath = front;
+            }
+            else if(CrossingDetectedLeft == true)
+            {
+                ChosenPath = left;
+            }
+            else
+            {
+                ChosenPath = turnaround;
+            }
+            PathSelected = true;
+            SuppressPathUpdate = true;
         }
+        else if(   (TurnFinished == false)
+                && (PathSelected == true)
+               )
+        {
+            ROS_INFO("Entered path switch-case");
+            switch(ChosenPath)
+            {
+            case left:
+                ROS_INFO("Entered left case");
+                turn(87.0, turnSpeed, 0);
+                ROS_INFO("Finished left case");
+                break;
+            case front:
+                TurnFinished = true;
+                break;
+            case right:
+                turn(87.0, turnSpeed, 1);
+                break;
+            case turnaround:
+                ROS_INFO("Entered turnaround case");
+                turn(177.0, turnSpeed, 1);
+                ROS_INFO("Finished turnaround case");
+                break;
+            default:
+                break;
+            }
+        }
+        else
+        {
+
+        }
+        break;
+    case moving:
+        set_velocities(stopSpeed, forwardSpeed);
+        PathSelected = false;
+        ChosenPath   = none;
+        TurnFinished = false;
+
+        if(   (   (CrossingDetectedRight == false)
+               && (CrossingDetectedLeft  == false)
+              )
+           || (CrossingDetectedLeftLL  != CrossingDetectedLeft)
+           || (CrossingDetectedRightLL != CrossingDetectedRight)
+          )
+        {
+            SuppressPathUpdate = false;
+        }
+        break;
+    default:
+        break;
     }
-    else
+}
+
+void set_velocities(float lin_vel, float ang_vel)
     {
-        switch(RobotStateLastLoop)
-        {
-        case moving:
-            if(RobotState == position_determination)
-            break;
-        default:
-            break;   
-        }
+        geometry_msgs::Twist msg;
+        msg.linear.x = lin_vel;
+        msg.angular.z = ang_vel;
+
+        cmd_vel_pub_.publish(msg);
     }
-}
 
-void Jhonny5_execute_order(void)
+void turn(float angleDeg, double angularSpeed, int direction)
 {
+    float angleRad = angleDeg*M_PI/180;
+    float endAngleRad = YawCurrent - angleRad;
+    if (endAngleRad < -M_PI)
+    {
+       endAngleRad = M_PI + (endAngleRad + M_PI);
+    }
+    ros::Rate rate(10);
+    while(YawCurrent > endAngleRad)
+    {
+        /* slow down turn to avoid as much overshoot as possible*/
+        if(endAngleRad >= (0.7*YawCurrent))
+        {
+            angularSpeed = turnSpeedSlow;
+        }
+        /* set turning direction */
+        if(direction == 0)
+        {
+            set_velocities(angularSpeed, stopSpeed);
+        }
+        else
+        {
+            set_velocities(-angularSpeed, stopSpeed);
+        }
+        
+        ros::spinOnce();
+        rate.sleep();
 
+    }
+    set_velocities(stopSpeed, stopSpeed);
+    TurnFinished = true;
 }
 
-void finishTileCallback(const std_msgs::BoolConstPtr& str)
+void getOdom(const nav_msgs::Odometry::ConstPtr& msg)
 {
-    FinishTileDetected = (bool)str;
+     tf::Quaternion quat;
+     tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
+
+     tf::Matrix3x3(quat).getRPY(RollCurrent,PitchCurrent,YawCurrent);
+     //ROS_INFO("roll=%f, pitch=%f, yaw=%f ",RollCurrent,PitchCurrent,YawCurrent);
 }
 
-void wallProximityCallback(const std_msgs::BoolConstPtr& str)
-{
-    WallDetectedFront = (bool)str;
-}
-
-void crossingLeftCallback(const std_msgs::BoolConstPtr& str)
-{
-    CrossingDetectedLeft = (bool)str;
-}
-
-void crossingRightCallback(const std_msgs::BoolConstPtr& str)
-{
-    CrossingDetectedRight = (bool)str;
-}
-
-void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+void laserScanFrontCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
      int i;
-     float sum = 0;
-     avgrange = 0;
+     float sumfront = 0;
+
      for(i=0; i<10; i++)
      {
-         lsranges[i] = scan->ranges[i];
-         sum += lsranges[i];
-         printf("Range data %d : %1.8f\n",i, lsranges[i]);
+         LsFrontRanges[i] = scan->ranges[i];
+         sumfront += LsFrontRanges[i];
+         //printf("Front range data %d : %1.8f\n",i, LsFrontRanges[i]);
      }
-     avgrange = sum/10;
-     printf("Average: %1.8f\n",avgrange);
+     //if (RobotState == moving) //freeze while turning
+     {
+        AvgFrontRange = sumfront/10;
+     }
+
+     if(AvgFrontRange > (wallCloseThreshold + wallCloseOffset))
+     {
+        WallDetectedFront = false;
+     }
+     else if(AvgFrontRange < wallCloseThreshold)
+     {
+        WallDetectedFront = true;
+     }
+     else{}
+     //ROS_INFO("Front average: %1.8f\n",AvgFrontRange);
+     //ROS_INFO("Wall Front: %d\n", WallDetectedFront);
+}
+
+void laserScanLeftCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+     int j;
+     float sumleft = 0;
+
+     for(j=0; j<10; j++)
+     {
+         LsLeftRanges[j] = scan->ranges[j];
+         sumleft += LsLeftRanges[j];
+         //printf("Left range data %d : %1.8f\n",j, LsLeftRanges[j]);
+     }
+     //if (RobotState == moving) //freeze while turning
+     {
+        AvgLeftRange = sumleft/10;
+     }
+
+     CrossingDetectedLeftLL = CrossingDetectedLeft;
+
+     if(AvgLeftRange >= (wallCloseThreshold + wallCloseOffset))
+     {
+         if(CrossConfirmTimerLeft > 0)
+         {
+             CrossConfirmTimerLeft--;
+         }
+         else
+         {
+             CrossingDetectedLeft = true;
+         }
+     }
+     else if(AvgLeftRange <= wallCloseThreshold)
+     {
+         CrossingDetectedLeft = false;
+         CrossConfirmTimerLeft = crossConfirmedTime;
+     }
+     else{}
+
+
+     //ROS_INFO("Left average: %1.8f\n",AvgLeftRange);
+     //ROS_INFO("CrossLeft: %d\n", CrossingDetectedLeft);
+}
+
+void laserScanRightCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+     int k;
+     float sumright = 0;
+
+     for(k=0; k<10; k++)
+     {
+         LsRightRanges[k] = scan->ranges[k];
+         sumright += LsRightRanges[k];
+         //printf("Right range data %d : %1.8f\n",k, LsRightRanges[k]);
+     }
+     //if (RobotState == moving) //freeze while turning
+     {
+        AvgRightRange = sumright/10;
+     }
+
+     CrossingDetectedRightLL = CrossingDetectedRight;
+
+     if(AvgRightRange >= (wallCloseThreshold + wallCloseOffset))
+     {
+         if(CrossConfirmTimerRight > 0)
+         {
+             CrossConfirmTimerRight--;
+         }
+         else
+         {
+             CrossingDetectedRight = true;
+         }  
+     }
+     else if(AvgRightRange <= wallCloseThreshold)
+     {
+         CrossingDetectedRight = false;
+         CrossConfirmTimerRight = crossConfirmedTime;
+     }
+     else{}
+     //ROS_INFO("Right average: %1.8f\n",AvgRightRange);
+     //ROS_INFO("CrossRight: %d\n", CrossingDetectedRight);
 }
--- src/jhonny5/jhonny5_main.h
+++ src/jhonny5/jhonny5_main.h
@@ -1,50 +1,78 @@
 #ifndef JHONNY5_MAIN_HH
 #define JHONNY5_MAIN_HH
 
-#include "ros/ros.h"
-#include "std_msgs/Bool.h" 
-#include "std_msgs/String.h" 
+#include <ros/ros.h>
+#include <std_msgs/Bool.h> 
+#include <std_msgs/String.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <actionlib/server/simple_action_server.h>
 #include <sensor_msgs/LaserScan.h>
 #include <gazebo/gazebo.hh>
+#include <tf/tf.h>
 #include <sstream>
 
 typedef enum robot_states_e
 {
     init = 0,
     position_determination,
-    moving,
-    end_state,
-    error_state
+    moving
 } robot_states_t;
 
-#define False (0)
-#define True  (1)
-//typedef actionlib::SimpleActionServer<jhonny5::MoveOrder> MA;
+typedef enum path_selection_e
+{
+    left = 0,
+    front = 1,
+    right = 2,
+    turnaround = 3,
+    none = 5
+} path_selection_t;
+
+#define wallCloseThreshold (2.00)
+#define wallCloseOffset (5.00)
+#define forwardSpeed (1.00)
+#define turnSpeed (0.25)
+#define turnSpeedSlow (0.1)
+#define stopSpeed (0.00)
+#define crossConfirmedTime (20)
 
 robot_states_t RobotState;
 robot_states_t RobotStateLastLoop;
-bool           FinishTileDetected;
 bool           WallDetectedFront;
 bool           RobotStopped;
 bool           CrossingDetectedLeft;
+bool           CrossingDetectedLeftLL;
 bool           CrossingDetectedRight;
-bool           LocationCheckProcessDone;
-float          lsranges[10];
-float          avgrange;
-unsigned int   StopTimer;
-unsigned int   TurnTimer;
+bool           CrossingDetectedRightLL;
+float          LsFrontRanges[10];
+float          LsLeftRanges[10];
+float          LsRightRanges[10];
+float          AvgFrontRange;
+float          AvgLeftRange;
+float          AvgRightRange;
 bool           TurnFinished;
+path_selection_t ChosenPath;
+bool           PathSelected;
+bool           SuppressPathUpdate;
+unsigned int   CrossConfirmTimerLeft;
+unsigned int   CrossConfirmTimerRight;
+double         YawCurrent;
+double         RollCurrent;
+double         PitchCurrent;
 
+ros::Publisher cmd_vel_pub_;
+ros::Publisher wall_prox_pub_;
+ros::Publisher cross_left_pub_;
+ros::Publisher cross_right_pub_;
+
+/* Main functions*/
 void Jhonny5_init(void);
-void Jhonny5_input_processing(void);
 void Jhonny5_state_machine(void);
-void Jhonny5_execute_order(void);
+void set_velocities(float lin_vel, float ang_vel);
+void turn(float angleDeg, double angularSpeed, int direction);
 /* Callback functions */
-void finishTileCallback(const std_msgs::BoolConstPtr& str);
-void wallProximityCallback(const std_msgs::BoolConstPtr& str);
-void crossingLeftCallback(const std_msgs::BoolConstPtr& str);
-void crossingRightCallback(const std_msgs::BoolConstPtr& str);
-void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+void laserScanFrontCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+void laserScanLeftCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+void laserScanRightCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+void getOdom(const nav_msgs::Odometry::ConstPtr& msg);
 #endif /* JHONNY5_MAIN_HH */
